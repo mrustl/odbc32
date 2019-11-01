d75403fa544dc8ac79e6c6d0385fb06c38be667e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-11-01 18:48:58 +0100

    Avoid code duplication
    - Use new function create_odbc32_object() to
      create list with element "socket" and "ref"
      and give it class "odbc32"
    - In odbcConnectAccess2007(), create the list
      with elements "access.file", "uid", "pwd" only
      once and reuse it. Therefore, call
      RODBC::odbcConnectAccess2007 by means of
      do.call().

--- R/odbc32.R
+++ R/odbc32.R
@@ -1,2 +1,16 @@
 #' @exportClass odbc32
 "_PACKAGE"
+
+#' Create Object of Class "odbc32"
+#'
+#' @param socket socket
+#' @param ref reference returned by \code{\link[r2r]{eval_remote}}
+#'
+create_odbc32_object <- function(socket, ref) {
+
+  result <- list(socket = socket, ref = ref)
+
+  class(result) <- "odbc32"
+
+  result
+}
